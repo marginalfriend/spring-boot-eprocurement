9dff04281ba78992bc4ec68b1ebb42601f65eeb5
Sheila Nuur Ditrie <snditrie@gmail.com>
2024-05-14 16:17:39 +0700

    [CREATE] create entity product supply

--- src/main/java/com/gacortech/eprocurement/entity/ProductSupply.java
+++ src/main/java/com/gacortech/eprocurement/entity/ProductSupply.java
@@ -1,4 +1,30 @@
 package com.gacortech.eprocurement.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product_supply")
 public class ProductSupply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @JoinColumn(name = "product_id", nullable = false)
+    private Products productId;
+
+    @ManyToOne
+    @JoinColumn(name = "vendor_id", nullable = false)
+    private Vendors vendorId;
+
+    @Column(name = "stock")
+    private Integer stock;
+
+    @Column(name = "price")
+    private Integer price;
 }
