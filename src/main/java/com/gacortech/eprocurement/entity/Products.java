176aaf7f40227a60e1a0f89a8dd7181fa18290ef
Sheila Nuur Ditrie <snditrie@gmail.com>
2024-05-14 16:09:20 +0700

    [CREATE] create entity products and categories

--- src/main/java/com/gacortech/eprocurement/entity/Products.java
+++ src/main/java/com/gacortech/eprocurement/entity/Products.java
@@ -1,4 +1,24 @@
 package com.gacortech.eprocurement.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
 public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Categories categoryId;
 }
