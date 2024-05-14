a2884400c0bb85ecc27b0fda0c9cade7d73bd50e
Jhiro Faran <jhiraf07@gmail.com>
2024-05-14 16:19:57 +0700

    [NEW] Entity Vendors

--- src/main/java/com/gacortech/eprocurement/entity/Vendors.java
+++ src/main/java/com/gacortech/eprocurement/entity/Vendors.java
@@ -1,4 +1,24 @@
 package com.gacortech.eprocurement.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name= "vendors")
 public class Vendors {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "vendor_name",nullable = false)
+    private String nameVendor;
 }
