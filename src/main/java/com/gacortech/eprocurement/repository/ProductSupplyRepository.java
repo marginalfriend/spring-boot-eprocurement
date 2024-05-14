ed3847f2cbb917d703e560cea2796afd633281a4
Jhiro Faran <jhiraf07@gmail.com>
2024-05-14 17:29:50 +0700

    [NEW] add new repositories ProdcustSupply and Vendors

--- src/main/java/com/gacortech/eprocurement/repository/ProductSupplyRepository.java
+++ src/main/java/com/gacortech/eprocurement/repository/ProductSupplyRepository.java
@@ -1,6 +1,5 @@
 package com.gacortech.eprocurement.repository;
 
-import com.gacortech.eprocurement.entity.ProductSupplies;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
