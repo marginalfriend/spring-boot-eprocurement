7d6c7af297ab0a871ad334126af62e20182ebd76
Abdullah <abdullahpsg@gmail.com>
2024-05-14 18:58:30 +0700

    [NEW] Order DTO

---
+++ src/main/java/com/gacortech/eprocurement/dto/request/Order.java
@@ -0,0 +1,17 @@
+package com.gacortech.eprocurement.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Order {
+    private String id;
+    private LocalDate orderDate;
+}
--- src/main/java/com/gacortech/eprocurement/dto/request/ProductSupply.java
+++ src/main/java/com/gacortech/eprocurement/dto/request/ProductSupply.java
@@ -1,2 +1,12 @@
-package com.gacortech.eprocurement.dto.request;public class ProductDetail {
+package com.gacortech.eprocurement.dto.request;
+
+import com.gacortech.eprocurement.entity.Products;
+import com.gacortech.eprocurement.entity.Vendors;
+
+public class ProductSupply {
+    private Integer id;
+    private Products productId;
+    private Vendors vendorId;
+    private Integer stock;
+    private Integer price;
 }
