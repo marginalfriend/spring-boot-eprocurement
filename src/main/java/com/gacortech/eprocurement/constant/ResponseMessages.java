538f0204f20ed60360f33a3639177d2c0e4a2a8f
Abdullah <abdullahpsg@gmail.com>
2024-05-14 16:11:47 +0700

    [NEW] response messages

---
+++ src/main/java/com/gacortech/eprocurement/constant/ResponseMessages.java
@@ -0,0 +1,11 @@
+package com.gacortech.eprocurement.constant;
+
+public class ResponseMessages {
+    public static final String SUCCESS_SAVE_DATA        = "Data created successfully";
+    public static final String SUCCESS_GET_DATA         = "Data found";
+    public static final String SUCCESS_UPDATE_DATA      = "Data updated successfully";
+    public static final String SUCCESS_DELETE_DATA      = "Data deleted successfully";
+    public static final String ERROR_NOT_FOUND          = "Data not found";
+    public static final String ERROR_INTERNAL_SERVER    = "Internal server error";
+}
+
