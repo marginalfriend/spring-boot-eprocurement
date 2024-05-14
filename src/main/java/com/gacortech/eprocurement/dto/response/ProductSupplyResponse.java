ca977fb0870db69a85a0ad8151ccc60614b90b09
Abdullah <abdullahpsg@gmail.com>
2024-05-14 21:02:45 +0700

    [NEW] ProductSupplyResponse

---
+++ src/main/java/com/gacortech/eprocurement/dto/response/ProductSupplyResponse.java
@@ -0,0 +1,9 @@
+package com.gacortech.eprocurement.dto.response;
+
+public class ProductSupplyResponse {
+    Integer id;
+    String  productName;
+    String  vendorName;
+    Integer stock;
+    Integer price;
+}
