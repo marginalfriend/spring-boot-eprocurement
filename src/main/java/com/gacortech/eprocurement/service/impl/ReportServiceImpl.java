cc226145c22af4d206cc6824d76f914c2e7ec5d7
Abdullah <abdullahpsg@gmail.com>
2024-05-16 06:08:51 +0700

    [NEW] Report endpoint

--- src/main/java/com/gacortech/eprocurement/service/impl/ReportServiceImpl.java
+++ src/main/java/com/gacortech/eprocurement/service/impl/ReportServiceImpl.java
@@ -16,7 +16,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
