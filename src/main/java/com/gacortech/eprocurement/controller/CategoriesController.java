3723292278c3fd2aeee035d3e274675d7256185f
Sheila Nuur Ditrie <snditrie@gmail.com>
2024-05-15 18:21:58 +0700

    [CHANGE] validation done

--- src/main/java/com/gacortech/eprocurement/controller/CategoriesController.java
+++ src/main/java/com/gacortech/eprocurement/controller/CategoriesController.java
@@ -1,12 +1,15 @@
 package com.gacortech.eprocurement.controller;
 
+import com.gacortech.eprocurement.constant.ResponseMessages;
 import com.gacortech.eprocurement.constant.Routes;
 import com.gacortech.eprocurement.dto.entity_rep.Category;
 import com.gacortech.eprocurement.dto.response.CategoryResponse;
 import com.gacortech.eprocurement.dto.response.CommonResponse;
 import com.gacortech.eprocurement.entity.Categories;
 import com.gacortech.eprocurement.service.CategoriesService;
+import com.gacortech.eprocurement.utils.ValidationUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,32 +17,59 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path = "/api/v0/categories")
+@RequestMapping(path = Routes.CATEGORIES)
 public class CategoriesController {
     private final CategoriesService categoriesService;
+    private final ValidationUtil validationUtil;
 
     @PostMapping
-    public CategoryResponse addNewCategory(@RequestBody Category request){
-        return categoriesService.create(request);
+    public ResponseEntity<CommonResponse<CategoryResponse>> addNewCategory(@RequestBody Category request){
+        validationUtil.validate(request);
+        CategoryResponse newCategory = categoriesService.create(request);
+        CommonResponse<CategoryResponse> response = CommonResponse.<CategoryResponse>builder()
+                .statusCode(HttpStatus.CREATED.value())
+                .message(ResponseMessages.SUCCESS_SAVE_DATA)
+                .data(newCategory)
+                .build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public List<CategoryResponse> showAllCategories(
+    public ResponseEntity<CommonResponse<List<CategoryResponse>>> showAllCategories(
             @RequestParam(name = "name", required = false) String name
     ){
-        Category category = Category.builder()
+        Category request = Category.builder()
                 .name(name)
                 .build();
-        return categoriesService.getAll(category);
+        List<CategoryResponse> getCategory = categoriesService.getAll(request);
+        CommonResponse<List<CategoryResponse>> response = CommonResponse.<List<CategoryResponse>>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(ResponseMessages.SUCCESS_GET_DATA)
+                .data(getCategory)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping(path = "/{id}")
-    public CategoryResponse getCategoryById(@PathVariable Integer id){
-        return categoriesService.getById(id);
+    @GetMapping(path = Routes.PATH_VAR_ID)
+    public ResponseEntity<CommonResponse<CategoryResponse>> getCategoryById(@PathVariable Integer id){
+        CategoryResponse getCategory = categoriesService.getById(id);
+        CommonResponse<CategoryResponse> response = CommonResponse.<CategoryResponse>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(ResponseMessages.SUCCESS_GET_DATA)
+                .data(getCategory)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping
-    public CategoryResponse updateCategories(@RequestBody Category request){
-        return categoriesService.update(request);
+    public ResponseEntity<CommonResponse<CategoryResponse>> updateCategories(@RequestBody Category request){
+        validationUtil.validate(request);
+        CategoryResponse updateCategory = categoriesService.update(request);
+        CommonResponse<CategoryResponse> response = CommonResponse.<CategoryResponse>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(ResponseMessages.SUCCESS_UPDATE_DATA)
+                .data(updateCategory)
+                .build();
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/gacortech/eprocurement/controller/ProductController.java
+++ src/main/java/com/gacortech/eprocurement/controller/ProductController.java
@@ -9,6 +9,7 @@ import com.gacortech.eprocurement.dto.response.PagingResponse;
 import com.gacortech.eprocurement.dto.response.ProductResponse;
 import com.gacortech.eprocurement.entity.Products;
 import com.gacortech.eprocurement.service.ProductsService;
+import com.gacortech.eprocurement.utils.ValidationUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -22,14 +23,22 @@ import java.util.List;
 @RequestMapping(path = Routes.PRODUCTS)
 public class ProductController {
     private final ProductsService productsService;
+    private final ValidationUtil validationUtil;
 
     @PostMapping
-    public ProductResponse addNewProduct(@RequestBody Product request){
-        return productsService.create(request);
+    public ResponseEntity<CommonResponse<ProductResponse>> addNewProduct(@RequestBody Product request){
+        validationUtil.validate(request);
+        ProductResponse newProduct = productsService.create(request);
+        CommonResponse<ProductResponse> response = CommonResponse.<ProductResponse>builder()
+                .statusCode(HttpStatus.CREATED.value())
+                .message(ResponseMessages.SUCCESS_SAVE_DATA)
+                .data(newProduct)
+                .build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public ResponseEntity<CommonResponse<List<Products>>> showAllProduct(
+    public ResponseEntity<CommonResponse<List<ProductResponse>>> showAllProduct(
             @RequestParam(name = "name", required = false) String name,
             @RequestParam(name = "categoryId", required = false) Integer categoryId,
             @RequestParam(name = "page", defaultValue = "1") Integer page,
@@ -48,6 +57,13 @@ public class ProductController {
 
         Page<Products> allProduct = productsService.getAll(request);
 
+        List<ProductResponse> productResponses = allProduct.getContent().stream()
+                .map(prd -> new ProductResponse(
+                        prd.getId(),
+                        prd.getName(),
+                        prd.getCategory().getId()
+                )).toList();
+
         PagingResponse pagingResponse = PagingResponse.builder()
                 .totalPages(allProduct.getTotalPages())
                 .totalElements(allProduct.getTotalElements())
@@ -57,22 +73,35 @@ public class ProductController {
                 .hasPrevious(allProduct.hasPrevious())
                 .build();
 
-        CommonResponse<List<Products>> response = CommonResponse.<List<Products>>builder()
+        CommonResponse<List<ProductResponse>> response = CommonResponse.<List<ProductResponse>>builder()
                 .statusCode(HttpStatus.OK.value())
                 .message(ResponseMessages.SUCCESS_GET_DATA)
-                .data(allProduct.getContent())
+                .data(productResponses)
                 .paging(pagingResponse)
                 .build();
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping(path = "/{id}")
-    public ProductResponse getProductById(@PathVariable String id){
-        return productsService.getById(id);
+    @GetMapping(path = Routes.PATH_VAR_ID)
+    public ResponseEntity<CommonResponse<ProductResponse>> getProductById(@PathVariable String id){
+        ProductResponse getProduct = productsService.getById(id);
+        CommonResponse<ProductResponse> response = CommonResponse.<ProductResponse>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(ResponseMessages.SUCCESS_GET_DATA)
+                .data(getProduct)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping
-    public ProductResponse updateProduct(@RequestBody Product request){
-        return productsService.update(request);
+    public ResponseEntity<CommonResponse<ProductResponse>> updateProduct(@RequestBody Product request){
+        validationUtil.validate(request);
+        ProductResponse updateProduct = productsService.update(request);
+        CommonResponse<ProductResponse> response = CommonResponse.<ProductResponse>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(ResponseMessages.SUCCESS_UPDATE_DATA)
+                .data(updateProduct)
+                .build();
+        return ResponseEntity.ok(response);
     }
 }
